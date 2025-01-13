e82b90b7683cbe1054e074b54609d48f93b1e021
Your Name <you@example.com>
2025-01-13 11:04:59 +0200

    Updated test test_get_all_projects

--- project/tests/unit/test_projects_unit.py
+++ project/tests/unit/test_projects_unit.py
@@ -1,21 +1,21 @@
-import datetime
+from datetime import datetime
 
 from app.api.routers import projects
 
 
-def test_get_project(test_app, monkeypatch):
+def test_get_all_projects(test_app, monkeypatch):
     test_data = [
         {
             "id": 1,
             "name": "test_name",
             "comment": "test_comment",
-            "created_at": datetime.datetime.now(datetime.timezone.utc),
+            "created_at": datetime(2024, 12, 1).isoformat(),
         },
         {
             "id": 2,
             "name": "test_name_2",
             "comment": "test_comment_2",
-            "created_at": datetime.datetime.now(datetime.timezone.utc),
+            "created_at": datetime(2024, 1, 1).isoformat(),
         },
     ]
 
@@ -27,3 +27,8 @@ def test_get_project(test_app, monkeypatch):
     response = test_app.get("/projects/")
 
     assert response.status_code == 200
+    assert response.json() == test_data
+
+
+def test_post_project():
+    pass
