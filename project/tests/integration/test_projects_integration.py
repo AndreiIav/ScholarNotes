63e12984738cb972e394abab4f6f9f0e8d0fe6b5
Your Name <you@example.com>
2025-01-13 15:16:02 +0200

    Added tests for post project endpoint

--- project/tests/integration/test_projects_integration.py
+++ project/tests/integration/test_projects_integration.py
@@ -1,17 +1,31 @@
 import json
+from datetime import datetime, timezone
 
 
-def test_add_project(test_app):
+def test_add_new_project(test_app):
     payload = {"name": "test_name", "comment": "test_comment"}
 
     res = test_app.post("/projects", data=json.dumps(payload))
 
     assert res.status_code == 201
+    assert res.json()["id"]
     assert res.json()["name"] == "test_name"
     assert res.json()["comment"] == "test_comment"
+    assert res.json()["created_at"]
+
+    created_at = res.json()["created_at"]
+    # create a datetime object from the "created_at" string
+    created_at_datetime = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%S.%fZ")
+    # make it UTC "aware"
+    created_at_datetime = created_at_datetime.replace(tzinfo=timezone.utc)
+    # get the difference between the current UTC aware datetime and the one
+    # retrieved from the response
+    seconds_diff = datetime.now(timezone.utc) - created_at_datetime
+    # it shouldn't be more than one second
+    assert seconds_diff.seconds <= 1
 
 
-def test_cannot_add_same_project_name(test_app):
+def test_cannot_add_a_project_with_already_existing_name(test_app):
     payload = {"name": "test_name_1", "comment": "test_comment_1"}
 
     test_app.post("/projects", data=json.dumps(payload))
--- project/tests/unit/test_projects_unit.py
+++ project/tests/unit/test_projects_unit.py
@@ -1,6 +1,33 @@
+import json
 from datetime import datetime
 
 from app.api.routers import projects
+
+
+def test_post_project_with_correct_payload(test_app, monkeypatch):
+    test_request_payload = {"name": "test_name", "comment": "test_comment"}
+    test_response_payload = {
+        "id": 1,
+        "name": "test_name",
+        "comment": "test_comment",
+        "created_at": datetime(2024, 12, 1).isoformat(),
+    }
+
+    async def mock_post_project(fake_payload, fake_db_session):
+        return test_response_payload
+
+    monkeypatch.setattr(projects, "post_project", mock_post_project)
+
+    response = test_app.post("/projects", data=json.dumps(test_request_payload))
+
+    assert response.status_code == 201
+    assert response.json() == test_response_payload
+
+
+def test_post_project_with_incorrect_payload(test_app):
+    response = test_app.post("/projects", data=json.dumps({}))
+
+    assert response.status_code == 422
 
 
 def test_get_all_projects(test_app, monkeypatch):
@@ -28,7 +55,3 @@ def test_get_all_projects(test_app, monkeypatch):
 
     assert response.status_code == 200
     assert response.json() == test_data
-
-
-def test_post_project():
-    pass
